4c0c29d3b417407f659048806283e9c0a4a2bdf5
r04522611 <r045226112ntu.edu.tw>
2017-06-20 19:05:44 +0800

    ver2

--- test/NP test.py
+++ test/NP test.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-a = [0, 2 , 4]
-b=[1,3,5]
-c= [7, 9,11]
-d = [8,10,12]
- 
-S= np.append([],[], axis=0)
+a = [0, 2, 4]
+b = [1, 3, 5]
+c = [7, 9, 11]
+d = [8, 10, 12]
+
+S = np.append([], [], axis=0)
 S.append(a)
-print (S)
+print(S)
+# ver 2
